c06a16636e9b6441cc23fdf4b228a060891dacce
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-07-15 19:18:16 +0000

    Added logger target with Spread in logger-builtin-spread/dump.lisp refs #422 * logger-builtin-spread/dump.lisp: new file; dumps a logger image with   preloaded cl-spread

---
+++ logger-builtin-spread/dump.lisp
@@ -0,0 +1,10 @@
+(load-system :cl-spread)
+(load-system :cl-rsb-tools-logger)
+
+(cffi:close-foreign-library 'spread::libspread)
+
+(defun reload-spread-and-main ()
+  (cffi:use-foreign-library spread::libspread)
+  (rsb.tools.logger:main))
+
+(com.dvlsoft.clon:dump "logger-builtin-spread" reload-spread-and-main)
