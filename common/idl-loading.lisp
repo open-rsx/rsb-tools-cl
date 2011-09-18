0238c93668a638adb56f94366a680391886fda49
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-09-18 12:47:19 +0000

    Arbitrary keyword arguments for load-idl in common/idl-loading.lisp * common/idl-loading.lisp (load-idl): added keyword parameters   pathname and dependency-handler; extended documentation string   accordingly   (load-idl t t): accept arbitrary keyword arguments   (load-idl pathname eql :file): likewise; pass to called method   (load-idl pathname eql :wild): likewise   (load-idl pathname eql :auto): likewise   (load-idl string t): likewise   (load-idl list t): likewise   (define-load-method): renamed define-text-method ->   define-load-method; additionally define methods for loading binary   descriptions; accept arbitrary keyword arguments; pass to called   method

--- common/idl-loading.lisp
+++ common/idl-loading.lisp
@@ -23,16 +23,29 @@
 ;;; IDL loading protocol
 ;;
 
-(defgeneric load-idl (source kind)
+(defgeneric load-idl (source kind
+		      &key
+		      pathname
+		      dependency-handler
+		      &allow-other-keys)
   (:documentation
    "Try to load an IDL definition from SOURCE assuming source is of
-the kind designated by KIND."))
+the kind designated by KIND.
+
+PATHNAME should be supplied, if SOURCE does not have an associated
+pathname (e.g. not-file-based-stream) to allow but filename-based
+heuristic should still be applied.
+
+If supplied, DEPENDENCY-HANDLER has to be a function that resolves
+dependencies. It is called when SOURCE contains references to entities
+not specified within SOURCE."))
 
 
 ;;;
 ;;
 
-(defmethod load-idl ((source t) (kind t))
+(defmethod load-idl ((source t) (kind t)
+		     &key &allow-other-keys)
   "Signal an error if we no not know how load an IDL for the
 combination SOURCE and KIND."
   (failed-to-load-idl
@@ -41,45 +54,60 @@ combination SOURCE and KIND."
 		   :format-control  "~@<~A is an unknown data definition type.~@:>"
 		   :format-arguments (list kind))))
 
-(defmethod load-idl ((source pathname) (kind (eql :file)))
+(defmethod load-idl ((source pathname) (kind (eql :file))
+		     &rest args
+		     &key &allow-other-keys)
   "Load an IDL definition from the file SOURCE. The IDL kind is
 inferred form the file type of SOURCE."
   (log1 :info "Processing IDL file ~S" source)
   (handler-bind
       ((error #'(lambda (condition)
 		  (failed-to-load-idl source condition))))
-    (load-idl source (make-keyword (string-upcase
-				    (pathname-type source))))))
+    (apply #'load-idl
+	   source (make-keyword (string-upcase
+				 (pathname-type source)))
+	   args)))
 
-(defmethod load-idl ((source pathname) (kind (eql :wild)))
+(defmethod load-idl ((source pathname) (kind (eql :wild))
+		     &rest args
+		     &key &allow-other-keys)
   "Load IDL definitions from all files matching the wildcard pathname
 SOURCE."
   (log1 :info "Processing IDL files matching ~S" source)
-  (map 'list (rcurry #'load-idl :file) (directory source)))
+  (map 'list #'(lambda (file)
+		 (apply #'load-idl file :file args))
+       (directory source)))
 
-(defmethod load-idl ((source pathname) (kind (eql :auto)))
+(defmethod load-idl ((source pathname) (kind (eql :auto))
+		     &rest args
+		     &key &allow-other-keys)
   "Load IDL definitions from the pathname SOURCE treating it as a wild
 pathname, as directory or as a file depending on its properties."
   (cond
     ((eq (pathname-name source) :wild)
-     (load-idl source :wild))
+     (apply #'load-idl source :wild args))
     ((null (pathname-name source))
      (append
-      (load-idl (merge-pathnames "*.proto" source) :wild)
-      (load-idl (merge-pathnames "*.protobin" source) :wild)))
+      (apply #'load-idl (merge-pathnames "*.proto" source) :wild args)
+      (apply #'load-idl (merge-pathnames "*.protobin" source) :wild args)))
     (t
-     (load-idl source :file))))
+     (apply #'load-idl source :file args))))
 
-(defmethod load-idl ((source string) (kind t))
+(defmethod load-idl ((source string) (kind t)
+		     &rest args
+		     &key &allow-other-keys)
   "Treat SOURCE as an URI if it contains a ':', treat it as a pathname
 otherwise."
   (if (find #\: source)
-      (load-idl (puri:parse-uri source) kind)
-      (load-idl (parse-namestring source) kind)))
+      (apply #'load-idl (puri:parse-uri source) kind args)
+      (apply #'load-idl (parse-namestring source) kind args)))
 
-(defmethod load-idl ((source list) (kind t))
+(defmethod load-idl ((source list) (kind t)
+		     &rest args
+		     &key &allow-other-keys)
   "Process all elements of SOURCE sequentially."
-  (map 'list (rcurry #'load-idl kind) source))
+  (map 'list #'(lambda (source) (apply #'load-idl source kind args))
+       source))
 
 
 ;;; Protocol buffer specific stuff
@@ -92,14 +120,15 @@ otherwise."
       (pbb:emit descriptor :class)
     (pbb:emit descriptor :deserializer)))
 
-(macrolet ((define-text-method (type)
-	     `(defmethod load-idl ((source ,type) (kind (eql :proto)))
-		(process-descriptor (pbf:load/text source)))))
-  (define-text-method stream)
-  (define-text-method pathname))
+(macrolet ((define-load-method (type kind func)
+	     `(defmethod load-idl ((source ,type) (kind (eql ,kind))
+				   &rest args
+				   &key &allow-other-keys)
+		(log1 :info "Parsing data definition from ~A" source)
+		(process-descriptor (apply #',func source args)))))
+  (define-load-method string   :proto    pbf:load/text)
+  (define-load-method stream   :proto    pbf:load/text)
+  (define-load-method pathname :proto    pbf:load/text)
 
-(macrolet ((define-binary-method (type)
-	     `(defmethod load-idl ((source ,type) (kind (eql :protobin)))
-		(process-descriptor (pbf:load/binary source)))))
-  (define-binary-method stream)
-  (define-binary-method pathname))
+  (define-load-method stream   :protobin pbf:load/binary)
+  (define-load-method pathname :protobin pbf:load/binary))
