49b4353af1df1c498d4b31e54634632dd53d05f5
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-09-27 07:58:10 +0000

    Simplified package definition in logger/package.lisp * formatting/package.lisp (package rsb.formatting): qualified   `defpackage' form with cl package; remove additional `in-package'   form

--- formatting/package.lisp
+++ formatting/package.lisp
@@ -17,9 +17,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses>.
 
-(in-package :cl-user)
-
-(defpackage :rsb.formatting
+(cl:defpackage :rsb.formatting
   (:use
    :cl
    :alexandria
