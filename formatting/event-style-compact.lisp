c4f81995c2a0600a4459aa32735c56477101a565
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-02-15 02:37:19 +0000

    Simplifications in formatting/event-style-compact.lisp * formatting/event-style-compact.lisp (header): updated copyright   (define-compact-style): simplified local macro   (define-compact-style compact): use simplified macro   (define-compact-style compact+): likewise

--- formatting/event-style-compact.lisp
+++ formatting/event-style-compact.lisp
@@ -1,6 +1,6 @@
 ;;; event-style-compact.lisp --- Compact event formatting style class.
 ;;
-;; Copyright (C) 2011 Jan Moringen
+;; Copyright (C) 2011, 2012 Jan Moringen
 ;;
 ;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 ;;
@@ -20,8 +20,15 @@
 (cl:in-package :rsb.formatting)
 
 (macrolet
-    ((define-compact-style (name doc columns sub-styles)
-       (let ((class-name (symbolicate "STYLE-" name)))
+    ((define-compact-style (name &body doc-and-sub-styles)
+       (let+ ((class-name (symbolicate :style "-" name))
+	      ((&values sub-styles nil documentation)
+	       (parse-body doc-and-sub-styles :documentation t))
+	      ((&flet+ make-sub-style ((predicate . spec))
+		 `(cons
+		   ,predicate
+		   (make-instance 'columns-mixin
+				  :columns ',spec)))))
 	`(progn
 	   (defmethod find-style-class ((spec (eql ,name)))
 	     (find-class ',class-name))
@@ -31,55 +38,43 @@
 				  header-printing-mixin)
 	     ()
 	     (:default-initargs
-	      :columns    ,columns
-	      :sub-styles ,sub-styles)
-	     (:documentation ,doc))))))
+	      :sub-styles (list ,@(map 'list #'make-sub-style
+				       sub-styles)))
+	     ,@(when documentation
+	         `((:documentation ,documentation))))))))
 
   (define-compact-style :compact
       "This formatting style prints several properties of received
 events on a single line. It is designed to fit into 80 columns. Some
 events are formatted specially according to their role in a
 communication pattern."
-    '((:now   :width 26 :alignment :left)
-      :origin
-      (:scope :width 16)
-      (:data  :width 16)
-      :data-size
-      :newline)
-    `((,#'request-event?
-       .
-       ,(make-instance 'columns-mixin
-		       :columns '((:now    :width 26 :alignment :left)
-				  :origin
-				  (:call   :width 43)
-				  :newline)))
-      (,#'reply-event?
-       .
-       ,(make-instance 'columns-mixin
-		       :columns '((:now    :width 26 :alignment :left)
-				  :origin
-				  (:result :width 43)
-				  :newline)))
-      (,(constantly t) . :self)))
+    (#'request-event? . ((:now    :width 26 :alignment :left)
+			 :origin
+			 (:call   :width 43)
+			 :newline))
+    (#'reply-event?   . ((:now    :width 26 :alignment :left)
+			 :origin
+			 (:result :width 43)
+			 :newline))
+    ((constantly t)   . ((:now    :width 26 :alignment :left)
+			 :origin
+			 (:scope  :width 16)
+			 (:data   :width 16)
+			 :data-size
+			 :newline)))
 
   (define-compact-style :compact+
       "This formatting style prints several properties of received
 events on a single line. It is designed to fit into 128 columns. Some
 events are formatted specially according to their role in a
 communication pattern."
-    '(:now
-      :origin :sequence-number :id :method :scope :data :data-size
-      :newline)
-    `((,#'request-event?
-       .
-       ,(make-instance 'columns-mixin
-		       :columns '(:now
-				  :origin :sequence-number :id :call :data-size
-				  :newline)))
-      (,#'reply-event?
-       .
-       ,(make-instance 'columns-mixin
-		       :columns '(:now
-				  :origin :sequence-number :call-id :result :data-size
-				  :newline)))
-      (,(constantly t) . :self))))
+    (#'request-event? . (:now
+			 :origin :sequence-number :id :call :data-size
+			 :newline))
+    (#'reply-event?   . (:now
+			 :origin :sequence-number :call-id :result :data-size
+			 :newline))
+    ((constantly t)   . (:now
+			 :origin :sequence-number :id :method :scope
+			 :data :data-size
+			 :newline))))
