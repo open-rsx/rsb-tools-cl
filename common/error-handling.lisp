974e19ba134e0172bb9f0f8c199f78a8ef80b973
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-09-21 18:39:12 +0700

    Improved error-handling functions in common/error-handling.lisp
    refs #1170, fixes #1172
    
    * common/error-handling.lisp (abort/signal): made condition parameter
      mandatory; call `invoke-restart' with the restart object
      (continue/verbose): fixed logging format strings
      (maybe-relay-to-thread): do not provide an `abort' restart in the
      target thread; do not call the policy in the target thread if it
      would abort anyway; provide `abort/signal' restart in worker threads;
      renamed parameter strategy -> policy
      (ftype invoke-with-error-policy): fixed function name
      invoke-with-error-handling-strategy -> invoke-with-error-policy
      (invoke-with-error-policy): use `restart-case' instead of
      `restart-bind' to ensure resignaling outside the dynamic scope of
      the error policy which may otherwise apply relay or logging behavior
      a second time

--- common/error-handling.lisp
+++ common/error-handling.lisp
@@ -26,14 +26,14 @@
 ;;; Toplevel error handling strategies
 ;;
 
-(defun abort/signal (&optional condition)
+(defun abort/signal (condition)
   "Like `cl:abort' but signal CONDITION using the `abort/signal'
 RESTART.
 
 This function is intended to be used as toplevel error policy."
   (if-let ((abort/signal (find-restart 'abort/signal condition)))
-    (invoke-restart 'abort/signal condition)
-    (abort)))
+    (invoke-restart abort/signal condition)
+    (abort condition)))
 
 (defun continue/verbose (&optional condition)
   "Like `cl:continue' but log a warning if `cl:continue' restart is
@@ -42,74 +42,95 @@ established and a different warning if it does not.
 This function is intended to be used as toplevel error policy."
   (if-let ((restart (find-restart 'continue condition)))
     (progn
-      (log1 :warn "~@<Error encountered; Recovering via restart~
+      (log1 :warn "Error encountered; Recovering via restart~
 ~2&~<| ~@;~S: ~A~:>~
 ~2&. Further processing may yield partial or incorrect results.~@[ Error was:~
 ~2&~<| ~@;~A~:>~
-~2&.~]~:>"
+~2&.~]"
 	    (list (restart-name restart) restart)
 	    (when condition (list condition)))
       (invoke-restart restart))
-    (log1 :warn "~@<No ~S restart; Cannot recover~@[. Error:~
+    (log1 :warn "No ~S restart; Cannot recover~@[. Error:~
 ~2&~<| ~@;~A~:>~
-~2&.~]~:>"
+~2&.~]"
 	  'continue (when condition (list condition)))))
 
 
 ;;; Toplevel utility functions and macros
 ;;
 
-(defun maybe-relay-to-thread (strategy
+(defun maybe-relay-to-thread (policy
 			      &key
 			      (target-thread (bt:current-thread)))
-  "Return a function of one argument which executes STRATEGY but
+  "Return a function of one argument which executes POLICY but
 establishes a TRANSFER-ERROR restart unless executing in
 TARGET-THREAD. Invoking the TRANSFER-ERROR restart does not perform a
-non-local control transfer. That is, STRATEGY still has to handle the
+non-local control transfer. That is, POLICY still has to handle the
 condition after invoking the restart.
 
-STRATEGY has to accept a condition as its sole argument."
+POLICY has to accept a condition as its sole argument.
+
+TARGET-THREAD has to be a `bt:thread' object and defaults
+to (bt:current-thread)."
   #'(lambda (condition)
       (let ((thread (bt:current-thread)))
 	(if (eq thread target-thread)
 
 	    ;; When executing in TARGET-THREAD, the error cannot be
-	    ;; transferred.
-	   (restart-bind
-	       ((abort #'(lambda (condition))))
-	     (funcall strategy condition))
+	    ;; transferred. Thus, we install no restarts.
+	    ;;
+	    ;; When POLICY is `abort/signal', we can just unwind,
+	    ;; preserving the backtrace.
+	   (if (eq policy #'abort/signal)
+	       (log1 :info "Error policy ~S in thread ~A; unwinding normally."
+		     policy thread)
+	       (progn
+		 (funcall policy condition)
+		 ;; If POLICY did not handle CONDITION, abort
+		 ;; TARGET-THREAD, signaling CONDITION.
+		 (invoke-restart 'abort/signal condition)))
 
 	   ;; When executing in a different thread, the error can be
 	   ;; transferred to TARGET-THREAD.
 	   (progn
 	     (log1 :info "Applying error policy ~A in background thread ~A"
-		   strategy thread)
+		   policy thread)
 	     (restart-case
-		 (funcall strategy condition)
+		 (funcall policy condition)
 	       (abort (&optional condition)
-		 (log1 :info "Error policy ~A aborted in background thread ~A. Aborting in main thread."
-		       strategy thread)
+		 (declare (ignore condition))
 		 (bt:interrupt-thread
-		  target-thread #'(lambda ()
-				    (invoke-restart 'abort/signal condition)))))
-	     (log1 :warn "Aborting background thread ~A" thread)
+		  target-thread #'(lambda () (invoke-restart 'abort))))
+	       (abort/signal (condition)
+		 (log1 :warn "Error policy ~A aborted with condition ~
+in background thread ~A. Aborting with condition in main thread. ~
+Condition was:~
+~2&~<| ~@;~A~:>~
+~2&."
+		       policy thread (list condition))
+
+		 (bt:interrupt-thread
+		  target-thread #'
+		  (lambda ()
+		    (invoke-restart 'abort/signal condition)))))
+	     (log1 :info "Aborting background thread ~A" thread)
 	     (abort))))))
 
 (declaim (ftype (function (function function) *)
-		invoke-with-error-handling-strategy))
+		invoke-with-error-policy))
 
 (defun invoke-with-error-policy (policy thunk)
   "Call THUNK with `cl:abort' and `abort/signal' restarts
 established. When an error is signaled, call POLICY to (potentially)
 handle it."
-  (restart-bind
-      ((abort        #'(lambda ()
-			 (error "Aborted.")))
-       (abort/signal #'(lambda (condition)
-			 (error condition))))
-    (handler-bind
-	((error policy))
-      (funcall thunk))))
+  (restart-case
+      (handler-bind
+	  ((error policy))
+	(funcall thunk))
+    (abort ()
+      (error "Aborted."))
+    (abort/signal (condition)
+      (error condition))))
 
 (defmacro with-error-policy ((policy) &body body)
   "Execute BODY with `cl:abort' and `abort/signal' restarts
