26101e1b8abe265249cd4ec06242d4be3bbe8d27
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-10-08 17:23:27 +0200

    Shortened license, updated copyright in **/*.lisp, *.asd, CMakeLists.txt
    * **/*.lisp (header): shortened license; updated copyright
    * cl-rsb-common.asd: likewise
    * cl-rsb-stats.asd: likewise
    * cl-rsb-formatting.asd: likewise
    * cl-rsb-formatting-png.asd: likewise
    * rsb-formatting-and-rsb-common.asd: likewise
    * rsb-formatting-and-rsb-stats.asd: likewise
    * cl-rsb-tools-call.asd: likewise
    * cl-rsb-tools-info.asd: likewise
    * cl-rsb-tools-logger.asd: likewise
    * cl-rsb-tools-main.asd: likewise
    * cl-rsb-tools-send.asd: likewise
    * CMakeLists.txt: likewise

--- formatting/name-mixin.lisp
+++ formatting/name-mixin.lisp
@@ -1,21 +1,8 @@
-;;; name-mixin.lisp --- Mixin class for named column classes.
-;;
-;; Copyright (C) 2011 Jan Moringen
-;;
-;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
-;;
-;; This Program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This Program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses>.
+;;;; name-mixin.lisp --- Mixin class for named column classes.
+;;;;
+;;;; Copyright (C) 2011, 2013 Jan Moringen
+;;;;
+;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package :rsb.formatting)
 
