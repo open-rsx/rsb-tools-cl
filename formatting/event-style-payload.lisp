67124eafb109e95e734b081aae3c4091305cc2fe
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-11-20 16:28:57 +0100

    Added payload-style slot in formatting/event-style-payload.lisp
    * formatting/event-style-payload.lisp (style-payload::payload-style):
      new slot; stores the style that should be used to format payloads
      (shared-initialize :after style-payload t): new method; construct
      style instances according to payload-style initarg
      (format-event t style-payload t): use the specified payload style
      instead of just :raw
      (format-event event style-payload t): likewise

--- formatting/event-style-payload.lisp
+++ formatting/event-style-payload.lisp
@@ -1,4 +1,4 @@
-;;;; event-style-payload.lisp --- Formatting style that only print the payload.
+;;;; event-style-payload.lisp --- Formatting style that only processes the payload.
 ;;;;
 ;;;; Copyright (C) 2011, 2012, 2013 Jan Moringen
 ;;;;
@@ -10,21 +10,34 @@
   (find-class 'style-payload))
 
 (defclass style-payload (separator-mixin)
-  ()
+  ((payload-style :reader   style-payload-style
+                  :writer   (setf style-%payload-style)
+                  :documentation
+                  "Stores a style instance which should be used to
+                   format payloads."))
   (:documentation
    "Only format the payload of each event, but not the meta-data."))
+
+(defmethod shared-initialize :after ((instance   style-payload)
+                                     (slot-names t)
+                                     &key
+                                     (payload-style :raw))
+  (setf (style-%payload-style instance)
+        (case payload-style
+          (:raw payload-style)
+          (t    (make-style payload-style)))))
 
 (defmethod format-event ((event  t)
                          (style  style-payload)
                          (stream t)
                          &key &allow-other-keys)
-  (format-payload event :raw stream))
+  (format-payload event (style-payload-style style) stream))
 
 (defmethod format-event ((event  event)
                          (style  style-payload)
                          (stream t)
                          &key &allow-other-keys)
-  (format-payload (event-data event) :raw stream))
+  (format-payload (event-data event) (style-payload-style style) stream))
 
 (defmethod format-event :after ((event  t)
                                 (style  style-payload)
