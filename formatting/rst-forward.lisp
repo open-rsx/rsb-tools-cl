2248a102fea95eddc4d7bb0e592bddd65786e452
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-11-20 14:42:44 +0100

    RST forward definitions do not clobber real RST in formatting/rst-forward.lisp
    * formatting/rst-forward.lisp (toplevel): only define packages and
      classes if they do not already exist

--- formatting/rst-forward.lisp
+++ formatting/rst-forward.lisp
@@ -6,42 +6,42 @@
 
 ;;; rst.vision Types
 
-(cl:defpackage #:rst.vision
-  (:use
-   #:cl)
-
-  (:export
-   #:image
-   #:image-color-mode
-   #:image-width
-   #:image-height
-   #:image-data))
+(cl:eval-when (:compile-toplevel :load-toplevel :execute)
+  (cl:unless (cl:find-package '#:rst.vision)
+    (cl:defpackage #:rst.vision
+      (:export
+       #:image
+       #:image-color-mode
+       #:image-width
+       #:image-height
+       #:image-data))))
 
 (cl:in-package #:rst.vision)
 
-(defclass image ()
-  ((color-mode :reader image-color-mode)
-   (width      :reader image-width)
-   (height     :reader image-height)
-   (data       :reader image-data)))
+(cl:unless (cl:find-class 'image cl:nil)
+  (cl:defclass image ()
+    ((color-mode :reader image-color-mode)
+     (width      :reader image-width)
+     (height     :reader image-height)
+     (data       :reader image-data))))
 
 ;;; rst.audition Types
 
-(cl:defpackage #:rst.audition
-  (:use
-   #:cl)
-
-  (:export
-   #:sound-chunk
-   #:sound-chunk-channels
-   #:sound-chunk-rate
-   #:sound-chunk-sample-type
-   #:sound-chunk-data))
+(cl:eval-when (:compile-toplevel :load-toplevel :execute)
+  (cl:unless (cl:find-package '#:rst.audition)
+    (cl:defpackage #:rst.audition
+      (:export
+       #:sound-chunk
+       #:sound-chunk-channels
+       #:sound-chunk-rate
+       #:sound-chunk-sample-type
+       #:sound-chunk-data))))
 
 (cl:in-package #:rst.audition)
 
-(defclass sound-chunk ()
-  ((channels    :reader sound-chunk-channels)
-   (rate        :reader sound-chunk-rate)
-   (sample-type :reader sound-chunk-sample-type)
-   (data        :reader sound-chunk-data)))
+(cl:unless (cl:find-class 'sound-chunk cl:nil)
+  (cl:defclass sound-chunk ()
+    ((channels    :reader sound-chunk-channels)
+     (rate        :reader sound-chunk-rate)
+     (sample-type :reader sound-chunk-sample-type)
+     (data        :reader sound-chunk-data))))
