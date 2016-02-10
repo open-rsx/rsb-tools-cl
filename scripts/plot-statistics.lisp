f9efe985f78a00d8a2ca76bd913fff3378afb848
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-02-10 16:04:14 +0100

    Modernized scripts/plot-{statistics,timestamps}.lisp:
    * scripts/plot-statistics.lisp (header): updated copyright
      (toplevel): removed code for gnuplot < 4.6
    * scripts/plot-timestamps.lisp (header): updated copyright
      (toplevel): removed code for gnuplot < 4.6

--- scripts/plot-statistics.lisp
+++ scripts/plot-statistics.lisp
@@ -1,8 +1,10 @@
 ;;;; plot-statistics.lisp --- Plot timing-related statistics for multiple scopes.
 ;;;;
-;;;; Copyright (C) 2013, 2014, 2015 Jan Moringen
+;;;; Copyright (C) 2013, 2014, 2015, 2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+
+;;;; Requires Gnuplot version 4.6 or newer.
 
 (unless (boundp 'initialized?)
   (setf (symbol-value 'initialized?) t
@@ -12,14 +14,6 @@
         (bucket-size      2) ; seconds
         (type             "png")
         (terminal-options "font \",8\" size 1400, 800")
-        (gnuplot-4.6?     (ppcre:register-groups-bind (major minor)
-                              ("gnuplot ([0-9]+)\\.([0-9]+)"
-                               (with-output-to-string (stream)
-                                 (sb-ext:run-program "gnuplot" '("--version")
-                                                     :search t :output stream)))
-                            (when-let ((major (when major (parse-integer major)))
-                                       (minor (when minor (parse-integer minor))))
-                              (or (> major 4) (and (= major 4) (>= minor 6))))))
         ;; Internal variables
         (data-file        (format nil "/tmp/~8,'0X.txt" (random (expt 16 8))))
         (plot-file        (format nil "/tmp/~8,'0X.plt" (random (expt 16 8))))
@@ -39,9 +33,7 @@
     (defun save-statistics (file timestamp scopes)
       (with-output-to-file (stream file :if-does-not-exist :create
                                         :if-exists         :append)
-        (if gnuplot-4.6?
-            (format stream "~A" timestamp)
-            (format stream "~D" (timestamp->unix/nsec timestamp)))
+        (format stream "~A" timestamp)
         (mapcar (lambda+ ((&ign . (&ign . quantities)))
                   (mapcar (lambda (q)
                             (format stream "~{ ~F~}"
@@ -109,8 +101,8 @@
         (format stream "set terminal ~A ~@[~A~]
                         set output \"~A.~3:*~A\"~2*
 
-                        ~:[~:;set xdata time
-                        set timefmt \"%Y-%m-%dT%H:%M:%S+01:00\"~]
+                        set xdata time
+                        set timefmt \"%Y-%m-%dT%H:%M:%S+01:00\"
 
                         set xtics font \",6\"
                         set xtics rotate by -30
@@ -123,7 +115,7 @@
                         set grid
 
                         plot ~S"
-                type terminal-options output gnuplot-4.6? data-file)
+                type terminal-options output data-file)
         (iter (for (scope . (id . _)) in    (sort scopes #'string<
                                                   :key (compose #'scope-string #'car)))
               (for first?            :first t :then nil)
--- scripts/plot-timestamps.lisp
+++ scripts/plot-timestamps.lisp
@@ -1,8 +1,10 @@
 ;;;; plot-timestamps.lisp --- Plot timestamps for multiple scopes.
 ;;;;
-;;;; Copyright (C) 2013, 2014 Jan Moringen
+;;;; Copyright (C) 2013, 2014, 2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+
+;;;; Requires Gnuplot version 4.6 or newer.
 
 (unless (boundp 'initialized?)
   (setf (symbol-value 'initialized?) t
@@ -17,14 +19,6 @@
                                          #x0080ff #x00ff80))
         (type             "png")
         (terminal-options "font \",8\" size 1400, 800")
-        (gnuplot-4.6?     (ppcre:register-groups-bind (major minor)
-                              ("gnuplot ([0-9]+)\\.([0-9]+)"
-                               (with-output-to-string (stream)
-                                 (sb-ext:run-program "gnuplot" '("--version")
-                                                     :search t :output stream)))
-                            (when-let ((major (when major (parse-integer major)))
-                                       (minor (when minor (parse-integer minor))))
-                              (or (> major 4) (and (= major 4) (>= minor 6))))))
         ;; Internal variables
         (plot-file        (format nil "/tmp/~8,'0X.plt" (random (expt 16 8))))
         (scopes           (make-hash-table :test #'eq)))
@@ -49,10 +43,7 @@
                              scope scopes
                              (multiple-value-list (open-data-file))))
              (*print-pretty* nil))
-        (if gnuplot-4.6?
-            (format stream "~{~A~^, ~}~%" timestamps)
-            (format stream "~{~D~^, ~}~%"
-                    (mapcar #'timestamp->unix/nsec timestamps)))))
+        (format stream "~{~A~^, ~}~%" timestamps)))
 
     (defun write-plot-commands-for-scope (stream scope file timestamps index first?)
       (let+ ((first? first?)
@@ -73,8 +64,8 @@
         (format stream "set terminal ~A~@[ ~A~]
                         set output \"~A.~3:*~A\"~2*
 
-                        ~:[~:;set xdata time
-                        set timefmt \"%Y-%m-%dT%H:%M:%S+01:00\"~]
+                        set xdata time
+                        set timefmt \"%Y-%m-%dT%H:%M:%S+01:00\"
 
                         set xtics font \",6\"
                         set xtics rotate by -30
@@ -89,7 +80,7 @@
                         set grid
 
                         plot "
-                type terminal-options output gnuplot-4.6?)
+                type terminal-options output)
         (iter (for  (scope . (file)) in     (sort (hash-table-alist scopes) #'string<
                                                   :key (compose #'scope-string #'car)))
               (for  index            :from  0)
