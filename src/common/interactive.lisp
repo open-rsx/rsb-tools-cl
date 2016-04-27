62f2f903556b2fff64ac36a400baa2dcffde2052
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-04-27 12:52:18 +0200

    Refactored with-interactive-interrupt-exit macro in
    The heavy lifting is now done in the new function
    `call-with-interactive-interrupt-exit'.
    
    * src/common/interactive.lisp (header): updated copyright
      (call-with-interactive-interrupt-exit): new function; set up requested
      interrupt handlers
      (with-interactive-interrupt-exit): expand to a
      `call-with-interactive-interrupt-exit' call

--- src/common/interactive.lisp
+++ src/common/interactive.lisp
@@ -1,51 +1,61 @@
 ;;;; interactive.lisp --- Functions for interactive stuff.
 ;;;;
-;;;; Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016 Jan Moringen
+;;;; Copyright (C) 2011-2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:rsb.tools.common)
 
-(defmacro with-interactive-interrupt-exit ((&key
-                                            (signals       '(sb-posix:SIGINT
-                                                             sb-posix:SIGTERM))
-                                            (target-thread '(bt:current-thread)))
-                                           &body body)
+(defun call-with-interactive-interrupt-exit (thunk
+                                             &key
+                                             (signals       `(,sb-posix:SIGINT
+                                                              ,sb-posix:SIGTERM))
+                                             (target-thread (bt:current-thread)))
+  (restart-case
+      (labels
+          ((initial-handler (signal info context)
+             (declare (ignore info context))
+             (log:info "~@<Caught signal ~D; aborting thread ~
+                        ~A.~@:>"
+                       signal target-thread)
+             ;; Install a handler which warns about the repeated
+             ;; signal and then ignores it.
+             (sb-unix::enable-interrupt signal #'noop-handler)
+             ;; If TARGET-THREAD is being interrupted by the signal,
+             ;; just abort. Otherwise interrupt TARGET-THREAD with
+             ;; `abort'.
+             (if (eq (bt:current-thread) target-thread)
+                 (abort)
+                 (bt:interrupt-thread target-thread #'abort)))
+           (noop-handler (signal info context)
+             (declare (ignore info context))
+             (log:warn "~@<Caught signal ~D during signal-triggered ~
+                        shutdown; don't do this; ignoring the ~
+                        signal~@:>"
+                       signal))
+           (install-handler (signal)
+             #+win32 (declare (ignore signal))
+             #-win32
+             (sb-unix::enable-interrupt signal #'initial-handler)))
+        ;; Install signal handlers unless on windows.
+        #-win32 (mapcar #'install-handler signals)
+        (funcall thunk))
+    ;; Establish an `abort' restart for signal handlers to invoke
+    ;; (from TARGET-THREAD or other threads).
+    (abort ())))
+
+(defmacro with-interactive-interrupt-exit
+    ((&key
+      (signals       nil signals-supplied?)
+      (target-thread nil target-thread-supplied?))
+     &body body)
   "Run BODY with an interruption handler that exits non-locally and
    returns nil instead of entering the debugger."
   ;; This whole macro is mainly needed for two reasons:
   ;; 1. Protect against multiple signals
   ;; 2. Perform proper shutdown for signals other than SIGINT,
   ;;    especially SIGTERM.
-  (once-only (target-thread)
-    `(restart-case
-         (flet ((install-handler (signal)
-                  #+win32 (declare (ignore signal))
-                  #-win32
-                  (sb-unix::enable-interrupt
-                   signal
-                   (lambda (signal info context)
-                     (declare (ignore info context))
-                     ;; Install a handler which warns about the
-                     ;; recursive signal and then ignores it.
-                     (sb-unix::enable-interrupt
-                      signal
-                      (lambda (signal info context)
-                        (declare (ignore info context))
-                        (log:warn "~@<Caught signal ~D during ~
-                                   signal-triggered shutdown; don't do ~
-                                   this; ignoring the signal~@:>"
-                                  signal)))
-                     ;; If TARGET-THREAD is being interrupted by the
-                     ;; signal, just abort. Otherwise interrupt
-                     ;; TARGET-THREAD with `abort'.
-                     (if (eq (bt:current-thread) ,target-thread)
-                         (abort)
-                         (bt:interrupt-thread
-                          ,target-thread #'abort))))))
-           ;; Install signal handlers unless on windows.
-           #-win32 (mapcar #'install-handler (list ,@signals))
-           ,@body)
-       ;; Establish an `abort' restart for signal handlers to invoke
-       ;; (from TARGET-THREAD or other threads).
-       (abort ()))))
+  `(call-with-interactive-interrupt-exit
+    (lambda () ,@body)
+    ,@(when signals-supplied?       `(:signals ,signals))
+    ,@(when target-thread-supplied? `(:target-thread ,target-thread))))
