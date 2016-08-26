41b6a0254e5d81b7178df0db7dfd32b46f17e115
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-08-26 11:38:27 +0200

    Added command execution smoke test in test/commands/logger.lisp
    * test/commands/logger.lisp (test logger-root::smoke): new test; smoke
      test for executing the logger command

--- test/commands/logger.lisp
+++ test/commands/logger.lisp
@@ -53,3 +53,64 @@
          (ensure-condition missing-required-initarg (do-it)))
         (t
          (ensure (typep (princ-to-string (do-it)) 'string)))))))
+
+(addtest (logger-root
+          :documentation
+          "Smoke test of the `logger' command class.")
+  smoke
+
+  (let* ((scope               "/rsbtest/tools/commands/logger")
+         (configuration       *safe-configuration*)
+         (rsb:*configuration* configuration))
+    (ensure-cases (initargs events expected
+                   &key
+                   (expected-event-count (length events)))
+        `(;; Terminate after receiving and logging a single event on
+          ;; expected scope.
+          ((:uris       (,(puri:uri scope))
+            :style-spec (:programmable/template
+                         :template "${sequence-number} ${data}\\n"))
+           (,(rsb:make-event scope 1))
+           ,(format nil "0 1~%"))
+
+          ;; Terminate after receiving but filtering one event and
+          ;; receiving and logging a second.
+          ((:uris       (,(puri:uri scope))
+            :filters    (,(rsb.filter:filter :method :method nil))
+            :style-spec (:programmable/template
+                         :template "${sequence-number} ${data}\\n"))
+           (,(rsb:make-event scope 1)
+            ,(rsb:make-event scope 2 :method :unexpected))
+           ,(format nil "0 1~%")
+           :expected-event-count 1)
+
+          ;; Continue after an error.
+          ((:uris       (,(puri:uri scope))
+            :style-spec (:programmable/template
+                         :template ,(format nil "${(when (eql data 1) ~
+                                                     (error \"intentional error\"))} ~
+                                                 ${sequence-number} ${data}\\n")))
+           (,(rsb:make-event scope 1)
+            ,(rsb:make-event scope 2))
+           ,(format nil "NIL 1 2~%")))
+
+      (let* ((output  (make-string-output-stream))
+             (while   (lambda (i event)
+                        (declare (ignore event))
+                        (< i expected-event-count)))
+             (command (apply #'make-command :logger
+                             (append initargs `
+                                     (:stream ,output :while ,while))))
+             (sender  (bt:make-thread
+                       (lambda ()
+                         (sleep 1)      ; TODO racy
+                         (let ((rsb:*configuration* configuration))
+                           (rsb:with-participant (i :informer scope)
+                             (mapc (curry #'rsb:send i) events)))))))
+        (unwind-protect
+             (progn
+               (handler-bind ((error #'continue))
+                 (command-execute command :error-policy #'continue))
+               (ensure-same (get-output-stream-string output) expected
+                            :test #'string=))
+          (bt:join-thread sender))))))
